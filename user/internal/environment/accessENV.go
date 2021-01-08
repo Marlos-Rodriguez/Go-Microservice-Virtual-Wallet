4ce54df1239faaf628d64046e31cb0e42d4f8c2e
Marlos <Marlos2811@hotmail.com>
2021-01-08 13:55:02 -0300

    Nginx working, map error fix, finish 1 version

--- user/internal/environment/accessENV.go
+++ user/internal/environment/accessENV.go
@@ -2,25 +2,33 @@ package environment
 
 import (
 	"os"
+	"sync"
+
 	//Autoload the env
 	_ "github.com/joho/godotenv/autoload"
 )
 
-var environment = make(map[string]string)
+var (
+	environment      = map[string]string{}
+	environmentMutex = sync.RWMutex{}
+)
 
 //AccessENV Return the ENV if exits
 func AccessENV(key string) string {
+	environmentMutex.Lock()
 	if environment[key] != "" {
 		return environment[key]
 	}
+	environmentMutex.Unlock()
 
 	val := os.Getenv(key)
 
 	if val == "" || len(val) <= 0 {
 		return ""
 	}
-
+	environmentMutex.Lock()
 	environment[key] = val
+	environmentMutex.Unlock()
 
 	return val
 }
